99618650996f9e19b7bd3b9c9e701b8e023e035c
HerbertHe <Herbert.He0229@gmail.com>
2021-05-18 21:08:01 +0800

    test :white_check_mark: : update test

--- src/core/time.test.ts
+++ src/core/time.test.ts
@@ -1,10 +1,9 @@
-import { resolve6 } from "dns"
 import { nginxTimeConverter } from "./time"
 
 test("测试nginx时间转化", () => {
     const ex = "13/May/2021:06:49:38 +0800"
     const res = nginxTimeConverter(ex)
-    expect(res?.toString()).toEqual(
-        "Thu May 13 2021 06:49:38 GMT+0800 (GMT+08:00)"
+    expect(res?.toLocaleString("en-US", { timeZone: "Asia/Shanghai" })).toEqual(
+        "5/13/2021, 6:49:38 AM"
     )
 })
